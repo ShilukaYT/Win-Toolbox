146aa77dcce21b4d4008701023902ff7d13f19d6
Hiếu GL Lite <doanhieudakwil@gmail.com>
2024-09-06 22:35:39 +0700

    Update WinToolbox.bat

--- WinToolbox.bat
+++ WinToolbox.bat
@@ -49,6 +49,56 @@ set Bin=%~dp0Bin
 set Temp=%~dp0Temp
 set Download=%~dp0Download
 
+::Set color
+:: NORMAL FOREG COLORS
+set nhcolorsetcolor=
+set Black=%nhcolorsetcolor%[30m
+set Red=%nhcolorsetcolor%[31m
+set Green=%nhcolorsetcolor%[32m
+set Yellow=%nhcolorsetcolor%[33m
+set Blue=%nhcolorsetcolor%[33m
+set Magenta=%nhcolorsetcolor%[35m
+set Cyan=%nhcolorsetcolor%[36m
+set White=%nhcolorsetcolor%[37m
+set Lightgray=%nhcolorsetcolor%[37m
+:: STRONG FOREG COLORS
+set SWhite=%nhcolorsetcolor%[90m
+set SRed=%nhcolorsetcolor%[91m
+set SGreen=%nhcolorsetcolor%[92m
+set SYellow=%nhcolorsetcolor%[93m
+set SBlue=%nhcolorsetcolor%[94m
+set SMagenta=%nhcolorsetcolor%[95m
+set SCyan=%nhcolorsetcolor%[96m
+set SWhite=%nhcolorsetcolor%[97m
+:: NORMALBACKGROUND
+set NBlack=%nhcolorsetcolor%[40m
+set NRed=%nhcolorsetcolor%[41m
+set NGreen=%nhcolorsetcolor%[42m
+set NYellow=%nhcolorsetcolor%[43m
+set NBlue=%nhcolorsetcolor%[44m
+set NMagenta=%nhcolorsetcolor%[45m
+set NCyan=%nhcolorsetcolor%[46m
+set NWhite=%nhcolorsetcolor%[47m
+:: STRONGBACKGROUND
+set BBlack=%nhcolorsetcolor%[100m
+set BRed=%nhcolorsetcolor%[101m
+set BGreen=%nhcolorsetcolor%[102m
+set BYellow=%nhcolorsetcolor%[103m
+set BBlue=%nhcolorsetcolor%[104m
+set BMagenta=%nhcolorsetcolor%[105m
+set BCyan=%nhcolorsetcolor%[106m
+set BWhite=%nhcolorsetcolor%[107m
+:: STYLE
+set Green=%nhcolorsetcolor%[32m
+set White=%nhcolorsetcolor%[37m
+set Cyan=%nhcolorsetcolor%[36m
+set Magenta=%nhcolorsetcolor%[35m
+set Red=%nhcolorsetcolor%[31m
+set Yellow=%nhcolorsetcolor%[33m
+set Lightgray=%nhcolorsetcolor%[37m
+set Bold=%nhcolorsetcolor%[1m
+
+set nhcolor=%Bin%\nhcolor.exe
 set wget=%Bin%\wget.exe
 set aria2c=%Bin%\aria2c.exe
 set SevenZip=%Bin%\7za.exe
@@ -70,28 +120,28 @@ goto :EOF
 :Main
 call :GetOSInfo
 call :CreUI
-Echo   Hello %USERNAME%, Computer Name: %COMPUTERNAME%
-echo   Current OS: Windows %winver% %Edition%, Build: %build% (%Ver%)
+%nhcolor% 07 "  %Red%Hello %USERNAME% %White%| %Yellow%Computer Name: %COMPUTERNAME%"
+%nhcolor% 07 "  %Green%Current OS: Windows %winver% %Edition% %White%| %Cyan%Build: %build% (%Ver%)"
 echo.
-echo %Line%
-echo.
-echo   
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.
-echo.
+echo ===============================================================================================
+echo                                    Application Installer
+%nhcolor% 07 " %Green%- Internet Browser                 - Game Client               - Android Games"
+%nhcolor% 07 " %Yellow%+ [1] Google Chrome                %Blue%+ [11] Steam                %Magenta%+ [23] Google Play Games"
+%nhcolor% 07 " %Yellow%+ [2] Microsoft Edge + webview2    %Blue%+ [12] Epic Games           %Magenta%+ [24] Free Fire MAX"
+%nhcolor% 07 " %Yellow%+ [3] Mozilla Firefox              %Blue%+ [13] Xbox                 %Magenta%+ [25] Clash of Clans"
+%nhcolor% 07 " %SWhite%- Zip and Unzip                    %Blue%+ [14] HoyoPlay             %SBlue%- System components"
+%nhcolor% 07 " + [4] WinRAR (Activated)           - Software Development      + [26] Visual C++ 2005 - 2022"
+%nhcolor% 07 " - Multimedia                       + [15] Visual Studio Code   + [27] DirectX
+%nhcolor% 07 " + [5] VLC Media player             + [16] Visual Studio        + [28] Windows App SDK
+%nhcolor% 07 " + [6] Windows Meida Player (UWP)   + [17] Github Desktop       - Virtual Machine
+%nhcolor% 07 " + [7] Microsoft Photos             + [18] Python               + [29] VMware Workstation Pro
+%nhcolor% 07 " - Office                           + [19] Sublime Text         + [30] Oracle VM VirtualBox
+%nhcolor% 07 " + [8] Microsoft Office             - Android Emulator          - Recording and Live Streaming
+%nhcolor% 07 " + [9] WPS Office                   + [20] BlueStacks           + [31] OBS Studio
+%nhcolor% 07 " - App Store                        + [21] LDPlayer             + [32] Streamlab OBS
+%nhcolor% 07 " + [10] Microsoft Store             + [22] MSI App Player       + [33] TikTok Live Studio
 echo %line%
-echo [Z] Subscribe to Hieu GL Lite
-echo [X] Quit
+%nhcolor% 07 " [Z] Subscribe to Hieu GL Lite                                         Page 1 - [N] Next Page"
+%nhcolor% 07 " [X] Quit"
 echo %line%
-pause
+set /p MainChoice=" %SWhite%Enter Your Choice and ENTER: "
